0469275c0e4527dba0d7d51704a779b8715932f8
enlore <nick@fog.haus>
2018-05-14 15:30:25 -0500

    Proxy setup

--- preact.config.js
+++ preact.config.js
@@ -4,7 +4,7 @@ const preactCliFlow     = require('preact-cli-plugin-flow');
 const Dotenv            = require('dotenv-webpack')
 require('dotenv').config()
 
-const DEBUG_WEBPACK = process.env.DEBUG_WEBPACK === "true" ? true : false
+const DEBUG_WEBPACK = process.env.DEBUG_WEBPACK === 'true' ? true : false
 
 export default function (config, env, helpers) {
   if (DEBUG_WEBPACK) {
@@ -13,24 +13,27 @@ export default function (config, env, helpers) {
     writeFileSync('__webpack_env', JSON.stringify(env, null, 2), 'utf8')
   }
 
-  // let sass = helpers.getLoadersByName(config, 'proxy-loader')[1]
-
-  // sass.loader.options.options.includePaths = [
-    // resolve(__dirname, 'node_modules'),
-    // resolve(__dirname, 'src/style')
-  // ]
-
-  // writeFileSync('__webpack_sass_loader_config', JSON.stringify(sass, null, 2), 'utf8')
-
+  if (process.env.DEV_PROXY_TO) {
+    // https://webpack.js.org/configuration/dev-server/#devserver-proxy
+    config.devServer.proxy = { '/api': process.env.DEV_PROXY_TO }
+  }
   // expose process.env to client code in dev
   config.node.process = 'mock'
 
   // https://github.com/mrsteele/dotenv-webpack
   const dotenv = new Dotenv({
-    path: './.env',           // load this now instead of the ones in '.env'
-    safe: false,              // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
-    systemvars: false,        // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
-    silent: false             // hide any errors
+    // specify alternative path to env file
+    path: './.env',
+
+    // attempt to load .env.example
+    safe: false,
+
+    // load all the predefined 'process.env' variables which will trump
+    // anything local per dotenv specs.
+    systemvars: false,
+
+    // hides errors
+    silent: false
   })
 
   console.info('fog-tools-preact-cli-app', 'dumping .env file vars', dotenv.definitions)
